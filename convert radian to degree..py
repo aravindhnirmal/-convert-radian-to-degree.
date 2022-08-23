228e8c71a1fff9249c4f40c1a696b481a5ad9445
nirmal aravindh <97661995+aravindhnirmal@users.noreply.github.com>
2022-08-23 20:52:01 +0530

    Update convert radian to degree..py

--- convert radian to degree..py
+++ convert radian to degree..py
@@ -1,4 +1,4 @@
-pi=22/7
-radian = float(input("Input radians: "))
+pi=22/7 #pi is store into a varible
+radian = float(input("Input radians: ")) #in this we take the input as float and  store in a varible name radian
 degree = radian*(180/pi)
-print(degree)
+print(degree) #print it 
